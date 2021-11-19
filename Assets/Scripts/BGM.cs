de3c3b27e30b39361e281a50fbbd179902cdc6c1
Anan Venkatesh <av1u19@soton.ac.uk>
2021-11-19 22:06:46 +0000

    UI changes

--- Assets/Scripts/BGM.cs
+++ Assets/Scripts/BGM.cs
@@ -8,14 +8,16 @@ public class BGM : MonoBehaviour
     public List<AudioClip> clips;
 
     // Start is called before the first frame update
-    IEnumerator Start()
+    void Start()
     {
         AudioSource BGMSource = GetComponent<AudioSource>();
         BGMSource.clip = clips[Random.Range(0, clips.Count)];
+        BGMSource.loop = true;
         BGMSource.Play();
-        yield return new WaitForSeconds(BGMSource.clip.length);
-        BGMSource.clip = clips[Random.Range(0, clips.Count)];
-        BGMSource.Play();
+        // If you want to revert to old behaviour, change to IEnumerator and v
+        // yield return new WaitForSeconds(BGMSource.clip.length);
+        // BGMSource.clip = clips[Random.Range(0, clips.Count)];
+        //BGMSource.Play();
     }
 
     // Update is called once per frame
